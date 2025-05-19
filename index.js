fd05f3f2105f992a8c662331f0baff72ba0ac94e
Faruq Bakare <olafaruqbakare@gmail.com>
2025-05-19 17:10:12 +0100

    updated frontend calls

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://waitlist.streamjump.info"],
+    origin: ["http://localhost:3000", "https://streamjump.info"],
     credentials: true,
   })
 );
